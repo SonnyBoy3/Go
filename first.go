f078672a68e61ff630d5ba87c98f7b2b895c4b29
Youssef Alaoui <87715668+SonnyBoy3@users.noreply.github.com>
2021-11-11 23:05:16 +0800

    Update first.go

--- first.go
+++ first.go
@@ -1,1 +1,39 @@
 
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	fmt.Println(`Enter the number of integers`)
+	var n int
+	
+	if m, err := Scan(&n); m != 1 {
+		panic(err)
+	}
+	
+	fmt.Println(n)
+	fmt.Println(`Enter the integers`)
+	all := make([]int, n)
+	ReadN(all, 0, n)
+	fmt.Println(all)
+}
+
+func ReadN(all []int, i, n int) {
+	if n == 0 {
+		return
+	}
+	if m, err := Scan(&all[i]); m != 1 {
+		panic(err)
+	}
+	ReadN(all, i+1, n-1)
+}
+
+func Scan(a *int) (int, error) {
+	return fmt.Fscan(r, a)
+}
+
+var r = strings.NewReader(`4
+10 20 30 50`)
